b7611923eaebc1f95a14a070cc19d75c9aeef640
Ignacio Orlando <ignacio.gonzalez@gruposuperdigital.com>
2021-06-21 02:10:43 -0300

    Subo a master la tab de modelos de negocios

--- TpPlataformasWeb/Controllers/HomeController.cs
+++ TpPlataformasWeb/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace TpPlataformasWeb.Controllers
             return View();
         }
 
+        public ActionResult ModeloDeNegocios()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
